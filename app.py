44ad7ee98ffc66759cbf11ee5c8e149a5086d11f
Bartosz Wiśniewski <bartoszwisniewski91@gmail.com>
2026-03-12 00:23:53 +0000

    Poprawka app.py w dev

--- app.py
+++ app.py
@@ -5,11 +5,60 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_devops():
-    return "<h1>Witaj w Projekcie Dyplomowym DevOps!</h1><p>Wersja: 1.0.0</p>"
+    version = "1.0.0"
+    return f"""
+    <html>
+    <head>
+        <title>DevOps Project</title>
+        <style>
+            body {{
+                font-family: Arial;
+                text-align: center;
+                background-color: #0f172a;
+                color: white;
+                margin-top: 80px;
+                overflow: hidden;
+            }}
+
+            h1 {{
+                font-size: 40px;
+            }}
+
+            .version {{
+                font-size: 20px;
+                margin-bottom: 40px;
+            }}
+
+            .container {{
+                font-size: 60px;
+                position: relative;
+                animation: move 6s linear infinite;
+            }}
+
+            @keyframes move {{
+                0%   {{ left: -200px; }}
+                100% {{ left: 100%; }}
+            }}
+        </style>
+    </head>
+    <body>
+
+        <h1>Witaj w Projekcie Dyplomowym DevOps!</h1>
+        <div class="version">Wersja aplikacji: {version}</div>
+
+        <div class="container">
+            🐳🚢⚙️
+        </div>
+
+        <p>Kontener DevOps w ruchu...</p>
+
+    </body>
+    </html>
+    """
 
 @app.route('/health')
 def health_check():
-    # Standardowy endpoint do sprawdzania stanu aplikacji przez K8s
+    # Endpoint do sprawdzania stanu aplikacji przez Kubernetes
     return jsonify(status="healthy", message="Aplikacja działa poprawnie"), 200
 
 if __name__ == '__main__':
